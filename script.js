25d9fb91079982a7765cd350e162a7dcd0e38825
Muhammad Hassan <hassanshakil12@gmail.com>
2023-09-06 22:47:25 +0500

    Mouse follower

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+
+const mouseFollower = (dets) =>{
+    window.addEventListener('mousemove', function(dets){
+        document.querySelector('.circle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+
+mouseFollower()
