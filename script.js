137c69bc4abe5daefb8843193a37326d847c51f9
Muhammad Hassan <hassanshakil12@gmail.com>
2023-09-07 23:25:55 +0500

    Starting animation

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ const mouseFollower = (dets) =>{
 }
 
 mouseFollower()
+
+const firstPageAnimation = () =>{
+    const tl = gsap.timeline()
+    tl.to('.nav .bounding h1', {
+        y: 0,
+        opacity: 1,
+        ease: Expo.easeInOut,
+        duration: 2,
+    })
+    .to('.page1 .top .bounding h1', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        stagger: 0.1,
+        delay: -2
+    })
+    .to('.page1 .top .bounding p', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1.5
+    })
+    .to('.page1 .middle .bounding h1', {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        stagger: 0.1,
+        delay: -1.8
+    })
+    .to('.page1 .bottom',{
+        opacity: 1,
+        delay: -1
+    })
+}
+
+firstPageAnimation()
